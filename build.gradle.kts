0a584a84d8011a9b9d76903d2c15064a562248bb
MCausc78 <mcausc78@gmail.com>
2024-11-08 14:11:35 +0200

    Replace placeholders in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,9 +40,9 @@ subprojects {
 
     // Fill out with your info
     aliucord {
-        author("DISCORD USERNAME", 123456789L)
-        updateUrl.set("https://raw.githubusercontent.com/USERNAME/REPONAME/builds/updater.json")
-        buildUrl.set("https://raw.githubusercontent.com/USERNAME/REPONAME/builds/%s.zip")
+        author("gatewaydisc.rdgg", 1073325901825187841L)
+        updateUrl.set("https://raw.githubusercontent.com/MCausc78/RNSucks/builds/updater.json")
+        buildUrl.set("https://raw.githubusercontent.com/MCausc78/RNSucks/builds/%s.zip")
     }
 
     android {
